4bf8875ac40aef918e1bc08bca94cc040f302d0e
ALLen <194188508+4139-Allen@users.noreply.github.com>
2026-07-18 22:59:15 +0800

    数据库设备名和位置唯一

--- 学习自动化测试/conftest.py
+++ 学习自动化测试/conftest.py
@@ -3,6 +3,8 @@ import pytest
 import requests
 import json
 
+from adodbapi.ado_consts import adNumeric
+
 BASE_URL = "https://www.doorlink.top"
 
 # ===============Fixture========================
@@ -28,6 +30,37 @@ def admin_client(client):
 def anon_client():
     """未登录的客户端（专门测试未认证场景）"""
     return requests.session()
+
+
+@pytest.fixture
+def register_user(admin_client, anon_client):
+    """创建一个测试用户，测试完后admin删除"""
+    import uuid
+    name = f"test-user-{uuid.uuid4().hex[:6]}"
+    resp = anon_client.post(
+        f"{BASE_URL}/api/auth/register",
+        json={"name":name, "password":}
+    )
+
+
+#===============设备=================
+@pytest.fixture
+def create_test_device(admin_client):
+    """创建一个临时设备，测试完后自动删除"""
+    import uuid
+    name = f"test-device-{uuid.uuid4().hex[:6]}"
+    resp = admin_client.post(
+        f"{BASE_URL}/api/devices",
+        json={"name":name, "location": "测试地址"}
+    )
+    device_id = resp.json()["data"]["device_id"]
+
+    yield device_id     # 把设备 ID 交给测试用例
+
+    #删除测试设备，保持环境干净
+    resp_del = admin_client.delete(f"{BASE_URL}/api/devices/{device_id}")  #清理测试设备
+
+    assert resp_del.status_code ==204
 
 
 
--- 学习自动化测试/test_device.py
+++ 学习自动化测试/test_device.py
@@ -1,5 +1,7 @@
 import pytest
 
+from 学习自动化测试.conftest import create_test_device
+
 BASE_URL = "https://www.doorlink.top"
 
 from test_utils.assert_util import assert_success, assert_failure
@@ -17,6 +19,27 @@ class TestDevice:
         """未登陆获取设备列表"""
         resp = anon_client.get(f"{BASE_URL}/api/devices")
         data = assert_failure(resp, 401, "未提供认证凭证")
+
+    @pytest.mark.destructive
+    def test_update_device_name(self, admin_client, create_test_device):
+        """测试修改设备名字/编号"""
+        resp = admin_client.put(f"{BASE_URL}/api/devices/{create_test_device}",
+                                json={"name": "测试改设备名", "location": "测试地址"}
+                                )
+        assert_success(resp)
+
+    @pytest.mark.destructive
+    def test_update_device_name_location(self, admin_client, create_test_device):
+        """测试修改设备名字和位置"""
+        resp = admin_client.put(f"{BASE_URL}/api/devices/{create_test_device}",
+                                json={"name": "测试改设备名", "location": "测试修改地址"}
+                                )
+        assert_success(resp)
+
+    @pytest.mark.destructive
+    def test_delete_device(self, admin_client, create_test_device):
+        """测试删除设备"""
+
 
 
 
